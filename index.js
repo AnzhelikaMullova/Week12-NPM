6728a87e8d9b41be3e9cfe353bb0fecf060e8436
AnzhelikaMullova <ulanova592@gmail.com>
2023-06-06 12:52:54 +0300

    commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+let button = document.getElementById('button');
+let numInput =document.getElementById('numberInput').value;
+
+let people = document.getElementById('people').value;
+let planets = document.getElementById('planets').value;
+let films = document.getElementById('films').value;
+
+
+function searchObj(){
+
+
+if( people !=="" ){ // если выбрана секция люди, то делаем feech запрос
+
+    fetch('https://swapi.dev/api/people/')
+    .then(response => response.json())
+    .then(json => {
+
+        json.forEach(element => {
+            console.log(element.name)
+            
+        });
+
+
+
+
+
+
+
+
+
+    })//скобки для закрытия then
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+button.addEventListener('click', searchObj);
+
