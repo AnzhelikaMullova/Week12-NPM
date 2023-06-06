cade9917b5c0f35b3e01f6d1ce678d87de47a63b
AnzhelikaMullova <ulanova592@gmail.com>
2023-06-06 14:48:33 +0300

    add

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 
 let button = document.getElementById('button');
 let numInput =document.getElementById('numberInput').value;
+let select=document.getElementById('select').value;
 
-let people = document.getElementById('people').value;
-let planets = document.getElementById('planets').value;
-let films = document.getElementById('films').value;
 
+
+const url = 'https://swapi.dev/api/${select}/${numInput}'
 
 function searchObj(){
 
-if( people !=="" ){ // если выбрана секция люди, то делаем feech запрос
+if( select !=="" ){ // если выбрана секция люди, то делаем feech запрос
 
-     fetch('https://swapi.dev/api/people/')
+    fetch('https://swapi.dev/api/')
     .then(response => response.json())
     .then(json => {
-       
+
 let results = json.results;
 console.log(results);
 
 results.forEach(element => {
 console.log(element.name)
 });
-
-       
-      
-
-
-
-
-
 
 
 
@@ -41,3 +33,6 @@ console.log(element.name)
 button.addEventListener('click', searchObj);
 
 
+// let people = document.getElementById('people').value;
+// let planets = document.getElementById('planets').value;
+// let films = document.getElementById('films').value;
