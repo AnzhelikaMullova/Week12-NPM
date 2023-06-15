ec67e47dff7a13d71c82086b0cbf65a9d9aadc66
AnzhelikaMullova <ulanova592@gmail.com>
2023-06-15 14:43:26 +0300

    add commit

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-
+// Получаем кнопку по ее ID
 let button = document.getElementById('button');
 
-
+// Функция которая будет вызываться при нажатии на кнопку
 function searchObj(){
+  //Получаем DOM элементы ввода номера и выбора результатов
     let container =document.getElementById('container');
-
     let numInput =document.getElementById('numberInput').value;
     let select=document.getElementById('select').value;    
-    const url = `https://swapi.dev/api/${select}/${numInput}`
+    //Создаем url на основе выбора и номера
+    const url = `https://swapi.dev/api/${select}/${numInput}`;
 
-    const newPromise = new Promise((resolve, reject) => {
-
-if(select !=="" && numInput !=="") 
-
+   
+//Проверяем условие что  категория и номер введены
+if(select !=="" && numInput !=="") {
+//Делаем http запрос к API
         fetch(url)
-        .then(response => response.json())
-
-        .then(json => {
-       container.innerHTML=`<p>${json.name}</p>`;
-          
+        .then((response )=> {
+          //Проверяем если ответ не успешный, то выбрасываем ошибку
+         if(!response.ok){
+          throw new Error(`ошибка: ${response.statusText}`);
+         }
+         //Преобразовываем ответ в JSON
+         return response.json();
         })
-        .catch(error => { //Если промис был отклонён
-
-          reject(error);
-        
-
-        });
-      });
-
-        newPromise.then(json => {
-         
-          
-    })
-    
-    
-    .catch(function (value) { // Если промис был отклонён
-  
-    //   /* Здесь параметр value будет хранить то значение,
-    //   которое было передано методу reject,
-    container.innerHTML=`<p>${value} + ', нам жаль :(')</p>`;
-     
-    }); 
+        .then((json)=>{
+          //Если выбрали фильмы то используем свойство title, в противном случае name
+          const displayText =select === "films" ? json.title : json.name;
+          //ВСтавляем текст в контейнер на странице
+          container.innerHTML =`<p>${displayText}</p>`;
+        })
+        .catch((error)=>{
+          container.innerHTML =`<p>${error}(</p>`;
+        })
+      }
+    }
+    //Добавляем обработчик события на кнопку
+    button/addEventListener("click",searchObj);
 
 
 
 
-}
-button.addEventListener('click', searchObj);
+
+
+
+
+
 
